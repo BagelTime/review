1e46904d6a9b65d754b15d6e8855eb46c58ff4ad
Alex Thomas <ajt11176@gmail.com>
2015-06-27 23:18:11 -0400

    redirect from / to /movies

--- review/urls.py
+++ review/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse
 
 import review.movie
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
-    #url(r'^$', RedirectView.get_redirect_url(self, 'movie:landing')),
-    #url(r'^movies/', include(..movie.urls), namespace='movie'),
+    url(r'^$', RedirectView.as_view(url='/movies/'), name='home'),
     url(r'^movies/', include('review.movie.urls', namespace='movie')),
 ]
