85ac0bbf80ad46d9938826238ab30b8a6f98934d
Alex Thomas <ajt11176@gmail.com>
2015-06-28 23:30:08 -0400

    basic search

--- review/movie/views.py
+++ review/movie/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from review.movie.forms import SearchForm
+import tmdbsimple as tmdb
+from django.conf import settings
 
-# Create your views here.
+
+#workaround for now for images
+image_base_size = 'http://image.tmdb.org/t/p/w342'
+
+def search(request):
+    form = SearchForm(request.POST)
+
+    if form.is_valid():
+        query = form.cleaned_data['search']
+        print "QUERY: %s" % query
+
+        tmdb.API_KEY = settings.TMDB_APIKEY
+        movieSearch = tmdb.Search()
+        movieSearch.movie(query=query)
+        results = []
+        for s in movieSearch.results:
+            result={'title': s['title'],
+                    'desc': s['overview'],
+                    'release_date': s['release_date'][:4],
+                    'poster': "%s%s" % (image_base_size, s['poster_path'])
+            }
+            results.append(result)
+
+        return render(request, 'movie/search_result.html', {'results': results})
