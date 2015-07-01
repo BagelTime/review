d832ac12d8d91630a04954e6977847c36416e10f
Alex Thomas <ajt11176@gmail.com>
2015-07-01 02:13:07 -0400

    Formating and styling

--- review/movie/views.py
+++ review/movie/views.py
@@ -4,7 +4,7 @@ import tmdbsimple as tmdb
 from django.conf import settings
 
 
-#workaround for now for images
+#w        return render(request, 'movie/search_result.html', {'results': results})orkaround for now for images
 image_base_size = 'http://image.tmdb.org/t/p/w342'
 
 def search(request):
@@ -17,13 +17,16 @@ def search(request):
         tmdb.API_KEY = settings.TMDB_APIKEY
         movieSearch = tmdb.Search()
         movieSearch.movie(query=query)
+
         results = []
         for s in movieSearch.results:
-            result={'title': s['title'],
-                    'desc': s['overview'],
-                    'release_date': s['release_date'][:4],
-                    'poster': "%s%s" % (image_base_size, s['poster_path'])
-            }
+            result = {}
+            result['title'] = s['title'] if s.has_key('title') and s['title'] != None else "<NO TITLE PROVIDED>"
+            result['desc'] = s['overview'] if s.has_key('overview') and s['overview'] != None else "<NO DESCRIPTION PROVIDED>"
+            result['release_date'] = s['release_date'][:4] if s.has_key('release_date') and s['release_date']!= None else "<NO DATE PROVIDED>"
+            result['poster_path'] = s['poster_path'] if s.has_key('poster_path') and s['poster_path'] != None else None
+
+
             results.append(result)
 
-        return render(request, 'movie/search_result.html', {'results': results})
+        return render(request, 'movie/search_result.html', {'results': results, 'poster_base': image_base_size})
