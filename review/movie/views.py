75ba1375d2531e7c044c4623ecc8b098607b4775
Alex Thomas <ajt11176@gmail.com>
2015-07-05 17:00:37 -0400

    Added rating

--- review/movie/views.py
+++ review/movie/views.py
@@ -22,11 +22,18 @@ def search(request):
         results = []
         for s in movieSearch.results:
             result = {}
+
+            movie = tmdb.Movies(s['id'])
+            rating_list = filter(lambda c: c['iso_3166_1'] == 'US', movie.releases()['countries'])
+            rating = rating_list[0]['certification'] if rating_list and rating_list[0]['certification'] else None
+
             result['title'] = s['title'] if s.has_key('title') and s['title'] != None else None
             result['desc'] = s['overview'] if s.has_key('overview') and s['overview'] != None else None
             result['release_date'] = s['release_date'][:4] if s.has_key('release_date') and s[
                                                                                                 'release_date'] != None else None
             result['poster_path'] = s['poster_path'] if s.has_key('poster_path') and s['poster_path'] != None else None
+            result['rating'] = rating
+
 
 
             results.append(result)
