51e8ccbc96bbc6845c6368d70ba582d5f0d4e2e6
Alex Thomas <ajt11176@gmail.com>
2015-07-12 02:58:38 +0000

    added tar with apache and bash configs

--- review/movie/views.py
+++ review/movie/views.py
@@ -6,7 +6,7 @@ from review.movie.models import Genre, Movie
 import tmdbsimple as tmdb
 
 
-#TODO replace with immage config later
+#TODO replace with image config later
 image_base_size = 'http://image.tmdb.org/t/p/w342'
 
 #TODO combine common pares of db and remote functions
@@ -14,11 +14,11 @@ image_base_size = 'http://image.tmdb.org/t/p/w342'
 def db_resulte(item):
     movie_list = Movie.objects.filter(db_id=item['id']).prefetch_related()
     if not movie_list:
-        print "Did not find  %s %s in DB" % (item['id'], item['title'])
+        #print "Did not find  %s %s in DB" % (item['id'], unicode(item['title']))
         return False
     else:
         movie = movie_list[0]
-        print "Found %s %s in DB" % (movie.title, movie.db_id)
+        #print "Found %s %s in DB" % (unicode(movie.title), movie.db_id)
         result = {
             'title' : movie.title,
             'rating': movie.rating,
@@ -33,18 +33,18 @@ def db_resulte(item):
 
 
 def remote_result(item):
-    print "Looking Up %s %s" % (item['id'], item['title'])
+    #print "Looking Up %s %s" % (item['id'], unicode(item['title']))
     movie = tmdb.Movies(item['id'])
     rating_list = filter(lambda c: c['iso_3166_1'] == 'US', movie.releases()['countries'])
     rating = rating_list[0]['certification'] if rating_list and rating_list[0]['certification'] else None
 
     #get genresgenre_ids
     genre_ids = item['genre_ids']
-    print "Genres: %s" % genre_ids
+    #print "Genres: %s" % genre_ids
     genres = None
     if genre_ids:
         genre_query = Q(pk=genre_ids.pop())
-        print genre_query
+        #print genre_query
         for g in genre_ids:
             genre_query |= Q(pk=g)
         genre_list = Genre.objects.filter(genre_query)
@@ -61,12 +61,12 @@ def remote_result(item):
     return result
 
 def search(request):
-    print "START SEARCH"
+    #print "START SEARCH"
     form = SearchForm(request.POST)
 
     if form.is_valid():
         query = form.cleaned_data['search']
-        print "QUERY: %s" % query
+        #print "QUERY: %s" % query
 
         #init tmdb object with API KEY
         tmdb.API_KEY = settings.TMDB_APIKEY
@@ -79,7 +79,7 @@ def search(request):
         results = []
         for s in movieSearch.results:
             # get movie rating for US only default to NR
-            print "%s: %s" % (s['id'], s['title'])
+            #print "%s: %s" % (s['id'], unicode(s['title']))
             result = db_resulte(s)
             if result:
                 results.append(result)
