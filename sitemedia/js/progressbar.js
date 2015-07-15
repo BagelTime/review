0882c871a530f17ac8b20e85301deaa5aefc3e89
Alex Thomas <ajt11176@gmail.com>
2015-07-14 21:44:51 -0400

    finished barcharts

--- sitemedia/js/progressbar.js
+++ sitemedia/js/progressbar.js
@@ -1,6 +1,6 @@
 $(function() {
-$( ".progressbar" ).each(function( index ) {
-var rate = parseInt($(this).attr('rate'));
-$(this).progressbar({value: rate, max: 10});
-});
+             $( ".progressbar" ).each(function( index ) {
+                 var rate = parseInt($(this).attr('rate'));
+                 $(this).progressbar({value: rate, max: 10});
+             });
 });
