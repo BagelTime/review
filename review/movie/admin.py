3283267911711de6cf3c0bdb636519b70bdd96f8
Alex Thomas <ajt11176@gmail.com>
2015-07-16 02:16:42 -0400

    fixed stats display

--- review/movie/admin.py
+++ review/movie/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from review.movie.models import Movie, Genre, Review
 
+class ReviewAdmin(admin.ModelAdmin):
+    save_as= True
+
 
 admin.site.register(Movie)
 admin.site.register(Genre)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
